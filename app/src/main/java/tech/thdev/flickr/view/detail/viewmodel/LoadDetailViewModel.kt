9e3715f6ead37db50e4c6bb475e67386250d166a
Taehwan <taehwan@thdev.net>
2018-12-12 21:36:08 +0900

    Detail ViewModel 테스트 케이스 추가 - 정상 케이스 - api 키 오류 테스트 - 포토 아이디가 없을 경우에 대한 테스트

--- app/src/main/java/tech/thdev/flickr/view/detail/viewmodel/LoadDetailViewModel.kt
+++ app/src/main/java/tech/thdev/flickr/view/detail/viewmodel/LoadDetailViewModel.kt
@@ -1,39 +1,39 @@
 package tech.thdev.flickr.view.detail.viewmodel
 
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import tech.thdev.coroutines.provider.DispatchersProvider
+import tech.thdev.coroutines.provider.DispatchersProviderSealed
 import tech.thdev.flickr.data.source.detail.DetailImageInfoRepository
 import tech.thdev.flickr.util.addComma
 import tech.thdev.support.base.coroutines.viewmodel.CoroutineScopeViewModel
 
-class LoadDetailViewModel(private val loadDetailRepository: DetailImageInfoRepository) : CoroutineScopeViewModel() {
+class LoadDetailViewModel(private val loadDetailRepository: DetailImageInfoRepository,
+                          private val dispatcher: DispatchersProviderSealed = DispatchersProvider) : CoroutineScopeViewModel() {
 
     lateinit var showErrorMessage: (message: String) -> Unit
     lateinit var setPhotoInfo: (title: String, tvDescription: String, ownerName: String, date: String, viewCount: String, commentCount: String) -> Unit
     lateinit var loadPhoto: (url: String, imageUrlLarge: String) -> Unit
 
-    fun loadDetail(photoId: String) {
-        launch {
-            loadDetailRepository.loadDetail(photoId, onError = {
-                launch(Dispatchers.Main) {
-                    showErrorMessage(it.message ?: "")
+    fun loadDetail(photoId: String) = launch {
+        loadDetailRepository.loadDetail(photoId, onError = {
+            launch(dispatcher.main) {
+                showErrorMessage(it.message ?: "")
+            }
+
+        }) { photoItem ->
+            launch(dispatcher.main) {
+                photoItem.photo.let { photo ->
+                    loadPhoto(photo.imageUrl, photo.imageUrlLarge)
                 }
 
-            }) { photoItem ->
-                launch(Dispatchers.Main) {
-                    photoItem.photo.let { photo ->
-                        loadPhoto(photo.imageUrl, photo.imageUrlLarge)
-                    }
-
-                    setPhotoInfo(
-                            photoItem.photo.title.content,
-                            photoItem.photo.description.content,
-                            photoItem.photo.owner.realName.takeIf { name -> !name.isEmpty() }
-                                    ?: photoItem.photo.owner.username,
-                            photoItem.photo.dates.taken,
-                            photoItem.photo.views.addComma(),
-                            photoItem.photo.comments.content.addComma())
-                }
+                setPhotoInfo(
+                        photoItem.photo.title.content,
+                        photoItem.photo.description.content,
+                        photoItem.photo.owner.realName.takeIf { name -> !name.isEmpty() }
+                                ?: photoItem.photo.owner.username,
+                        photoItem.photo.dates.taken,
+                        photoItem.photo.views.addComma(),
+                        photoItem.photo.comments.content.addComma())
             }
         }
     }
--- app/src/test/java/tech/thdev/flickr/view/detail/viewmodel/LoadDetailViewModelTest.kt
+++ app/src/test/java/tech/thdev/flickr/view/detail/viewmodel/LoadDetailViewModelTest.kt
@@ -1,15 +1,116 @@
 package tech.thdev.flickr.view.detail.viewmodel
 
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
+import tech.thdev.coroutines.provider.TestDispatchersProvider
+import tech.thdev.flickr.contract.FLICKR_DEFAULT_ADDRESS
 import tech.thdev.flickr.data.source.detail.DetailImageInfoRepository
-import tech.thdev.flickr.network.RetrofitCreate
+import tech.thdev.flickr.network.FlickrApi
+import tech.thdev.flickr.network.util.createRetrofit
 
 class LoadDetailViewModelTest {
 
-    private val viewModel = LoadDetailViewModel(DetailImageInfoRepository.getInstance(RetrofitCreate.flickrApi))
+    private lateinit var viewModel: LoadDetailViewModel
 
+    /**
+     * 함수에 대한 호출이 잘 일어나는지 확인한다.
+     */
     @Test
-    fun testLoadDetail() {
-        viewModel.loadDetail("44437878270")
+    fun testLoadDetail() = runBlocking {
+        viewModel = LoadDetailViewModel(DetailImageInfoRepository.getInstance(createRetrofit(FlickrApi::class.java, FLICKR_DEFAULT_ADDRESS) {
+            true
+        }), TestDispatchersProvider)
+
+        viewModel.run {
+            var isEnd = false
+            loadPhoto = { url, imageUrlLarge ->
+                assert(url.isNotEmpty())
+                assert(imageUrlLarge.isNotEmpty())
+            }
+
+            setPhotoInfo = { title, _, _, _, _, _ ->
+                assert(title.isNotEmpty())
+                isEnd = true
+            }
+
+            loadDetail("44437878270")
+
+            launch {
+                while (isActive) {
+                    if (isEnd) {
+                        break
+                    }
+                }
+            }.join()
+
+            // 대기 후 종료
+            assert(isEnd)
+        }
+    }
+
+    /**
+     * 함수에 대한 호출이 잘 일어나는지 확인하며, onError가 잘 동작하는지 체크한다.
+     */
+    @Test
+    fun testLoadDetailFailApiKey() = runBlocking {
+        viewModel = LoadDetailViewModel(DetailImageInfoRepository.getInstance(createRetrofit(FlickrApi::class.java, FLICKR_DEFAULT_ADDRESS) {
+            true
+        }, "apikey error"), TestDispatchersProvider)
+
+        viewModel.run {
+            var isEnd = false
+            showErrorMessage = {
+                assert(it.isNotEmpty())
+                isEnd = true
+            }
+
+            loadDetail("44437878270")
+
+            launch {
+                while (isActive) {
+                    if (isEnd) {
+                        break
+                    }
+                }
+            }.join()
+
+            // 대기 후 종료
+            assert(isEnd)
+        }
+    }
+
+    /**
+     * 함수에 대한 호출이 잘 일어나는지 확인하며, onError가 잘 동작하는지 체크한다.
+     */
+    @Test
+    fun testLoadDetailFailNotFoundId() = runBlocking {
+        viewModel = LoadDetailViewModel(DetailImageInfoRepository.getInstance(createRetrofit(FlickrApi::class.java, FLICKR_DEFAULT_ADDRESS) {
+            true
+        }), TestDispatchersProvider)
+
+        viewModel.run {
+            var isEnd = false
+            showErrorMessage = {
+                assert(it.isNotEmpty())
+                assert(it == "Photo not found")
+                isEnd = true
+            }
+
+            loadDetail("1920123123")
+
+            launch {
+                while (isActive) {
+                    if (isEnd) {
+                        break
+                    }
+                }
+            }.join()
+
+            // 대기 후 종료
+            assert(isEnd)
+        }
     }
 }
