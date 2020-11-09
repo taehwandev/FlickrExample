f4b60999bd2aad0335ccf3216e4d9adb4f932afd
taehwan <taehwan@thdev.net>
2020-11-09 18:10:54 +0900

    #10 Scope functions example

--- app/src/main/java/tech/thdev/flickr/view/detail/viewmodel/LoadDetailViewModel.kt
+++ app/src/main/java/tech/thdev/flickr/view/detail/viewmodel/LoadDetailViewModel.kt
@@ -15,11 +15,14 @@ class LoadDetailViewModel(
     lateinit var setPhotoInfo: (title: String, tvDescription: String, ownerName: String, date: String, viewCount: String, commentCount: String) -> Unit
     lateinit var loadPhoto: (url: String, imageUrlLarge: String) -> Unit
 
+    /**
+     * 4편 샘플 코드 - Scope Function 사용 시 알아두면 좋은 것
+     * 중첩 사용 시 주의할 부분을 알아 볼 수 있는 예, 2개의 중첩을 parameter로 접근하고있다.
+     */
     fun loadDetail(photoId: String) = viewModelScope.launch {
         loadDetailRepository.loadDetail(photoId, onError = {
             showErrorMessage(it.message ?: "")
             TestEvent.loadFail()
-
         }) { photoItem ->
             photoItem.photo.let { photo ->
                 loadPhoto(photo.imageUrl, photo.imageUrlLarge)
